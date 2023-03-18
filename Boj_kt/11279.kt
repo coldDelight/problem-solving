370df5681da05bd85efbfc1c80aa45e6d2717bc5
coldDelight <hno05039@naver.com>
2023-03-18 21:19:11 +0900

    sol 11279

---
+++ Boj_kt/11279.kt
@@ -0,0 +1,20 @@
+import java.util.Collections
+import java.util.PriorityQueue
+
+fun main() {
+    val priorityQueue = PriorityQueue<Int>(Collections.reverseOrder())
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuffer()
+    val n = br.readLine().toInt()
+    repeat(n) {
+        val x = br.readLine().toInt()
+        if (x == 0) {
+            sb.append(priorityQueue.poll() ?: 0)
+            sb.append("\n")
+        } else {
+            priorityQueue.add(x)
+        }
+    }
+    print(sb)
+    br.close()
+}
