cb4f97770d148534b61a9b9d26beffb6e693d313
coldDelight <hno05039@naver.com>
2023-03-23 22:27:41 +0900

    sol 11726

---
+++ Boj_kt/DP/11726.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readln().toInt()
+    val dp = IntArray(1001) { 0 }
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for (i in 3..n) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+    }
+    println(dp[n])
+}
